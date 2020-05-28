7da241f7296f848866690e038e824f48a419a007
rex <imsunzhiqi@outlook.com>
2020-05-28 15:07:42 +0800

    py1

---
+++ py1.py
@@ -0,0 +1,11 @@
+
+temp = input('guess what i am thinking about')
+guess = int(temp)
+
+if guess == 8:
+    print('u r right')
+    print('haha')
+else:
+    print('wrong choice')
+
+print('game is over')
